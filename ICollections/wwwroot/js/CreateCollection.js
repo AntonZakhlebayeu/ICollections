f54b86d17d64e10940268dc35dfd48eb58427dca
Anton <antonzakhlebayeu@gmail.com>
2022-04-22 15:33:16 +0300

    Update CreateCollection.js

--- ICollections/wwwroot/js/CreateCollection.js
+++ ICollections/wwwroot/js/CreateCollection.js
@@ -1,34 +1,37 @@
+const keys = ["alcohol", "books", "brand"];
+const classKeys = ["date", "brand"];
+
 const themeList = {
     "alcohol": {
-        "alcohol-date": {
+        "date": {
             "class": "lng-alcoholDate",
             "value": "alcoholDate",
         },
-        "alcohol-brand": {
+        "brand": {
             "class": "lng-alcoholBrand",
             "value": "alcoholBrand",
         },
     },
     
     "books": {
-        "books-date": {
+        "date": {
             "class": "lng-booksDate",
             "value": "booksDate",
         },
-        "books-author": {
+        "brand": {
             "class": "lng-booksAuthor",
             "value": "booksAuthor"
         }
     },
     
     "films": {
-        "films-date": {
+        "date": {
             "class": "lng-filmsDate",
-            "author": "filmsDate",
+            "value": "filmsDate",
         },
-        "films-author": {
+        "brand": {
             "class": "lng-filmsAuthor",
-            "author": "filmsAuthor",
+            "value": "filmsAuthor",
         }
     }
     
@@ -48,38 +51,33 @@ function changeContext() {
         additionalVision.display = "none";      
         return;
     }
-
+    
     additionalVision.display = "block";
     var additionalListInput = document.getElementsByClassName("form-check-input");
     var additionalListLabels = document.getElementsByClassName("form-check-label");
 
-    switch (selectedTheme)
-    {
-        case "alcohol":
-            additionalListLabels[0].classList.add(themeList[themeSelector.value]["alcohol-date"]["class"]);
-            additionalListInput[0].value = themeList[themeSelector.value]["alcohol-date"]["value"];
-            
-            additionalListLabels[1].classList.add(themeList[themeSelector.value]["alcohol-brand"]["class"]);
-            additionalListInput[1].value = themeList[themeSelector.value]["alcohol-brand"]["value"];
-            break;
-        case "books":
-            additionalListLabels[0].classList.add(themeList[themeSelector.value]["books-date"]["class"]);
-            additionalListInput[0].value = themeList[themeSelector.value]["books-date"]["value"];
-
-            additionalListLabels[1].classList.add(themeList[themeSelector.value]["books-author"]["class"]);
-            additionalListInput[1].value = themeList[themeSelector.value]["books-author"]["value"];
-            break;
-        case "films":
-            additionalListLabels[0].classList.add(themeList[themeSelector.value]["films-date"]["class"]);
-            additionalListInput[0].value = themeList[themeSelector.value]["films-date"]["value"];
-
-            additionalListLabels[1].classList.add(themeList[themeSelector.value]["films-author"]["class"]);
-            additionalListInput[1].value = themeList[themeSelector.value]["films-author"]["value"];
-            break;
-    }
-
-    additionalListLabels[2].classList.add("lng-CollectionComments");
-    additionalListInput[2].value = "CollectionComments";
+    changeClass(additionalListLabels, selectedTheme);
+    changeValue(additionalListInput, selectedTheme);
     
     changeLanguage();
 }    
+
+function changeClass(additionalListLabels, selectedTheme) {
+    for(let additionalListLabel in additionalListLabels)
+        for(var key in keys)
+            for(var classKey in classKeys) 
+                if(additionalListLabels[index].classList.contains(themeList[key][classKey]["class"]))
+                    additionalListLabel.classList.remove(themeList[key][classKey]["class"])
+
+    for(var i = 0; i < 2; i++) 
+        additionalListLabels[i].classList.add(themeList[selectedTheme][i]["class"])
+    
+    additionalListLabels[2].classList.add("lng-CollectionComments");
+}
+
+function changeValue(additionalListInputs, selectedTheme) {
+    for(var i = 0; i < 2; i++)
+        additionalListInputs[i].value = (themeList[selectedTheme][i]["value"])
+
+    additionalListInputs[2].value = "CollectionComments";
+}
