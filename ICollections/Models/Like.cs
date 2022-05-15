53ad985392ec6bbb4dcca44ce47785fa0359513e
Anton <antonzakhlebayeu@gmail.com>
2022-05-15 20:19:13 +0300

    Fixes for final.

--- ICollections/Models/Like.cs
+++ ICollections/Models/Like.cs
@@ -3,8 +3,6 @@ namespace ICollections.Models;
 public class Like
 {
     public int ItemId { get; init; }
-    public Item? Item { get; set; }
 
     public string UserId { get; init; } = null!;
-    public User? User { get; set; }
 }
